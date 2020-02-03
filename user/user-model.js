8da9d89f16c3a7314bcd85514cee636434a00fb3
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2020-02-03 10:05:18 -0800

    Commit 5: user-model done

---
+++ user/user-model.js
@@ -0,0 +1,42 @@
+const db = require('../database/dbConfig');
+
+module.exports = {
+    getAll,
+    findBy,
+    findById,
+    update,
+    remove,
+    add
+};
+
+const getAll = () => {
+    return db('users');
+};
+
+function findBy(filter) {
+    return db('users').where(filter);
+};
+
+function findById(id) {
+    return db('users')
+        .where({ id })
+        .first();
+};
+
+function update(id, changes) {
+    return db('users')
+        .where({ id })
+        .update(changes, '*');
+};
+
+function remove(id) {
+    return db('users')
+        .delete()
+        .where({ id });
+};
+
+async function add(user) {
+    return await db('users')
+        .insert(user)
+        .returning('*');
+};
